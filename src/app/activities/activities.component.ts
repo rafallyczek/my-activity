590c01989082582d1498137c04733a4b15ef4ecc
rafallyczek <rafal.lyczek@gmail.com>
2023-05-22 11:38:21 +0200

    Add activity model and display list of activities
    Added activity model file and list of activities displayed with
    ngFor directive (for now data is hardcoded).

--- src/app/activities/activities.component.ts
+++ src/app/activities/activities.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Activity } from './activity.model';
 
 @Component({
   selector: 'app-activities',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./activities.component.css']
 })
 export class ActivitiesComponent {
+
+  activityList: Activity[] = [
+    {
+      title: "Running",
+      currentStreak: 0,
+      longestStreak: 0
+    },
+    {
+      title: "Reading",
+      currentStreak: 7,
+      longestStreak: 7
+    },
+    {
+      title: "Listening practice",
+      currentStreak: 1,
+      longestStreak: 5
+    },
+  ];
 
 }
---
+++ src/app/activities/activity.model.ts
@@ -0,0 +1,5 @@
+export interface Activity {
+    title: string;
+    currentStreak: number;
+    longestStreak: number;
+}
