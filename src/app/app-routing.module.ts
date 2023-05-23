ba01f44879de45d85b8a0fac93c936ebc9532b8d
rafallyczek <rafal.lyczek@gmail.com>
2023-05-23 11:16:34 +0200

    Add navigation links list and router paths
    Added list of links to navbar and configured paths in
    routing module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { ActivitiesComponent } from "./activities/activities.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: "activities", component: ActivitiesComponent },
+    { path: "", redirectTo: "activities", pathMatch: "full" },
+    { path: "**", redirectTo: "activities" },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
