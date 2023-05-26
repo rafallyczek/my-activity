1b31d1851b0b8bb42798bd3d7e9ce3d75a0dab39
rafallyczek <rafal.lyczek@gmail.com>
2023-05-26 15:06:56 +0200

    Remove unused FormsModule from activity module

--- src/app/activity/activity.module.ts
+++ src/app/activity/activity.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ActivityRoutingModule } from './activity-routing.module';
 import { ActivityComponent } from './activity.component';
 import { ActivityAddComponent } from './activity-add/activity-add.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ActivityService } from './activity-service/activity.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
@@ -21,7 +21,6 @@ function initializeActivities(activityService: ActivityService){
     CommonModule,
     ActivityRoutingModule,
     FontAwesomeModule,
-    FormsModule,
     ReactiveFormsModule
   ],
   providers: [
