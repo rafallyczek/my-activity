671b65d45bf9c2e93cfe69a47bdcdcee40691fcb
rafallyczek <rafal.lyczek@gmail.com>
2023-06-02 18:51:53 +0200

    Replace activity list with observable
    Replaced activityList array in activity component with observable
    and removed subscribe/unsubscribe code (replaced with async pipe).

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -1,28 +1,23 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Activity } from './activity.model';
 import { ActivityService } from './activity-service/activity.service';
-import { Subscription } from 'rxjs';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-activities',
   templateUrl: './activity.component.html',
   styleUrls: ['./activity.component.css'],
 })
-export class ActivityComponent implements OnInit, OnDestroy {
+export class ActivityComponent implements OnInit {
   constructor(private activityService: ActivityService) {}
 
   addBtn = true;
   iconPlus = faPlus;
-  subscription!: Subscription;
-  activityList!: Activity[];
+  activityList$!: Observable<Activity[]>;
 
   ngOnInit(): void {
     this.loadActivities();
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
   }
 
   toggleAddBtn(){
@@ -34,9 +29,7 @@ export class ActivityComponent implements OnInit, OnDestroy {
   }
 
   loadActivities(){
-    this.subscription = this.activityService.getActivities().subscribe((data) => {
-      this.activityList = data;
-    });
+    this.activityList$ = this.activityService.getActivities();
   }
   
 }
