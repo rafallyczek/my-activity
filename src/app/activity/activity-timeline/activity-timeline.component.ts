61f7d415eb65ab04a2159ce8b761201a6483e996
rafallyczek <rafal.lyczek@gmail.com>
2023-07-17 18:10:55 +0200

    Implement toggleActivityRecord method
    Implemented toggleActivityRecord method (renamed from addActivityRecord
    as it now can also delete records) which calls addOrDeleteActivityRecord
    method from activity service when date is clicked. Also added settind
    of current date time to midnight in ngOnInit method (to make proper
    date comparison possible).

--- src/app/activity/activity-timeline/activity-timeline.component.ts
+++ src/app/activity/activity-timeline/activity-timeline.component.ts
@@ -1,24 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { sub } from 'date-fns';
+import { ActivityService } from '../activity-service/activity.service';
 
 @Component({
   selector: 'app-activity-timeline',
   templateUrl: './activity-timeline.component.html',
   styleUrls: ['./activity-timeline.component.css']
 })
-export class ActivityTimelineComponent {
+export class ActivityTimelineComponent implements OnInit {
 
   @Input() index = 0;
   date = new Date();
+
+  constructor(private activityService: ActivityService) {}
+
+  ngOnInit(): void {
+    this.date.setHours(0, 0, 0, 0);
+  }
 
   subDays(days: number): Date {
     return sub(this.date, { days: days });
   }
 
-  //TODO: implement adding activity record to activity's history array
-  addActivityRecord(date: Date): void {
-    console.log(date);
-    console.log(this.index);
+  toggleActivityRecord(date: Date): void {
+    this.activityService.addOrDeleteActivityRecord(this.index, date);
   }
 
 }
