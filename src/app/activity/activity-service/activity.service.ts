591a8de5897898cecc6ded4a90b40e7ea070e71b
rafallyczek <rafal.lyczek@gmail.com>
2023-07-26 11:04:31 +0200

    Implement streak calculation
    Implemented streak calculation methods in activity service:
    calculateCurrentStreak - calculates current streak of given activity,
    calculateLongestStreak - calculates longest streak of given activity,
    updateStreaks - calls calculateCurrentStreak and calculateLongestStreak
    methods to update currentStreak and longestStreak properties of activity
    (called every time user toggles activity record).

--- src/app/activity/activity-service/activity.service.ts
+++ src/app/activity/activity-service/activity.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { LocalStorageService } from 'src/app/local-storage/local-storage.service';
 import { Activity } from '../activity.model';
 import { Observable, map, of } from 'rxjs';
+import { sub, toDate } from 'date-fns';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ActivityService {
-  
+
   activities$!: Observable<Activity[]>;
 
   constructor(private localStorageService: LocalStorageService) {}
@@ -64,18 +65,93 @@ export class ActivityService {
   addOrDeleteActivityRecord(index: number, date: Date): void {
     let activity = this.getActivity(index);
 
-    if (activity == null){
+    if (activity == null) {
       return;
     }
 
-    if (!activity?.history.find(record => record.getTime() === date.getTime())){
+    if (!activity?.history.find((record) => record.getTime() === date.getTime())) {
       activity?.history.push(date);
     } else {
-      let recordIndex = activity?.history.findIndex(record => record.getTime() === date.getTime());
+      let recordIndex = activity?.history.findIndex((record) => record.getTime() === date.getTime());
       activity.history.splice(recordIndex, 1);
     }
 
+    activity.history = activity.history.sort((dateA, dateB) => dateB.getTime() - dateA.getTime());
+
+    activity = this.updateStreaks(activity);
+
     this.updateActivity(activity);
+  }
+
+  updateStreaks(activity: Activity): Activity {
+
+    activity.currentStreak = this.calculateCurrentStreak(activity);
+    activity.longestStreak = this.calculateLongestStreak(activity);
+    return activity;
+
+  }
+
+  calculateCurrentStreak(activity: Activity): number {
+
+    if (activity.history.length == 0){
+      return 0;
+    }
+
+    let today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    if (activity.history[0].getTime() !== today.getTime()){
+      return 0;
+    }
+
+    let date = activity.history[0];
+
+    let currentStreak = 1;
+
+    for (let i = 1; i<activity.history.length; i++){
+      let yesterday = sub(date, { days: 1 });
+      if (activity.history[i].getTime() === yesterday.getTime()) {
+        currentStreak += 1;
+        date = toDate(activity.history[i]);
+      } else {
+        break;
+      }
+    }
+
+    return currentStreak;
+
+  }
+
+  calculateLongestStreak(activity: Activity): number {
+
+    if (activity.history.length == 0){
+      return 0;
+    }
+
+    let date = activity.history[0];
+
+    let longestStreak = 0;
+    let count = 1;
+
+    for (let i = 1; i<activity.history.length; i++){
+      let yesterday = sub(date, { days: 1 });
+      if (activity.history[i].getTime() === yesterday.getTime()) {
+        count += 1;
+      } else {
+        if (count > longestStreak) {
+          longestStreak = count;
+        }
+        count = 1;
+      }
+      date = toDate(activity.history[i]);
+    }
+
+    if (count > longestStreak) {
+      longestStreak = count;
+    }
+
+    return longestStreak;
+
   }
 
 }
