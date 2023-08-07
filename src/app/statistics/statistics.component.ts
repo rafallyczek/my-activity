96224e92ec882531fbab0e0d641945e5307848a8
rafallyczek <rafal.lyczek@gmail.com>
2023-08-07 11:11:38 +0200

    Add activity list to statistics component
    Added activity list observable to statistics component (activities
    loaded in ngOnInit method).

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Activity } from '../activity/activity.model';
+import { ActivityService } from '../activity/activity-service/activity.service';
 
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.css']
 })
-export class StatisticsComponent {
+export class StatisticsComponent implements OnInit {
+
+  activityList$!: Observable<Activity[]>;
+
+  constructor(private activityService: ActivityService) {}
+
+  ngOnInit(): void {
+    this.activityList$ = this.activityService.getActivities();
+  }
 
 }
